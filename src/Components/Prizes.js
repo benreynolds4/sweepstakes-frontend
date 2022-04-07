76fdd3573b19278e6084d1b65d875abc2cc20dc1
benreynolds4 <ben.reynolds@ucdconnect.ie>
2022-04-07 18:05:32 +0100

    Remove 230 from pot because of casey fiasco

--- src/Components/Prizes.js
+++ src/Components/Prizes.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Prizes(props) {
     const { sweepstakesData, textColor } = props;
     let prizes = <React.Fragment>
-            <p><i className={`fa fa-money fa-fw w3-margin-right w3-large ${textColor}`} />1st Prize: €{Math.ceil((sweepstakesData.length * 10) * .6)}</p>
-            <p><i className={`fa fa-money fa-fw w3-margin-right w3-large ${textColor}`} />2nd Prize: €{Math.ceil((sweepstakesData.length * 10) * .10)}</p>
-            <p><i className={`fa fa-money fa-fw w3-margin-right w3-large ${textColor}`} />3nd Prize: €{Math.ceil((sweepstakesData.length * 10) * .08)}</p>
-            <p><i className={`fa fa-money fa-fw w3-margin-right w3-large ${textColor}`} />4th Prize: €{Math.ceil((sweepstakesData.length * 10) * .06)}</p>
-            <p><i className={`fa fa-money fa-fw w3-margin-right w3-large ${textColor}`} />5th Prize: €{Math.ceil((sweepstakesData.length * 10) * .03)}</p>
+            <p><i className={`fa fa-money fa-fw w3-margin-right w3-large ${textColor}`} />1st Prize: €{Math.ceil(((sweepstakesData.length * 10) - 230) * .6)}</p>
+            <p><i className={`fa fa-money fa-fw w3-margin-right w3-large ${textColor}`} />2nd Prize: €{Math.ceil(((sweepstakesData.length * 10) - 230) * .10)}</p>
+            <p><i className={`fa fa-money fa-fw w3-margin-right w3-large ${textColor}`} />3nd Prize: €{Math.ceil(((sweepstakesData.length * 10) - 230) * .08)}</p>
+            <p><i className={`fa fa-money fa-fw w3-margin-right w3-large ${textColor}`} />4th Prize: €{Math.ceil(((sweepstakesData.length * 10) - 230) * .06)}</p>
+            <p><i className={`fa fa-money fa-fw w3-margin-right w3-large ${textColor}`} />5th Prize: €{Math.ceil(((sweepstakesData.length * 10) - 230) * .03)}</p>
         </React.Fragment>
     return (
         <React.Fragment>
